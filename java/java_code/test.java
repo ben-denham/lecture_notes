0ad819e1d5f762eedd9368382806207f4a1fb1df
ben-denham <ben.denham@gmail.com>
2014-06-03 21:14:00 +1200

    Added java notes

--- java/java_code/test.java
+++ java/java_code/test.java
@@ -1,13 +1,50 @@
-class Box {
-    public final String test;
-    public Box () {
-	test = "adsf";
-	open();
+interface Beast {
+    public void bite();
+}
+
+abstract class Animal implements Beast {
+
+    String name;
+
+    public Animal() {
+	name = "Brumbie";
     }
+
+    public abstract void eat();
+
+    public void sleep() {
+	System.out.println("Animal..sleep");
+    }
+    public void prt() {
+	System.out.println(name);
+    }
+
+}
+
+class Cow extends Animal {
+
+    public Cow() {
+	name = "butterCup";
+    }
+
+    public void eat() {
+	System.out.println("Cow..eat");
+    }
+
+    public void bite() {
+	System.out.println("Cow..bite");
+    }
+
 }
 
 class Tester {
-    public static void main (String[] s) {
-	Box b = new Box();
+
+    public static void main(String[] args) {
+	Animal a = new Cow();
+	a.eat();
+	a.sleep();
+	a.bite();
+	a.prt();
     }
+
 }
